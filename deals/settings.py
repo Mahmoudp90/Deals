1e2961adff5a0698d24219df5a92d4e57a19e6cc
Mahmoud <Mahmoudp900@gmail.com>
2022-06-06 15:20:38 +0200

    Heruku local running v 1.0

--- deals/settings.py
+++ deals/settings.py
@@ -147,6 +147,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+# STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
